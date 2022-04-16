f531d4ad5565c1b0fd9aa067f79f43d9a379bb43
MrTomMus <75030876+MrTomMus@users.noreply.github.com>
2022-04-16 21:13:44 +0300

    update

--- src/components/Navbar/FindFriends/FindFriends-container.jsx
+++ src/components/Navbar/FindFriends/FindFriends-container.jsx
@@ -1,6 +1,46 @@
+import React from 'react';
 import { connect } from "react-redux";
 import { followAc, unFollowAc, setUsersAc, setPageAC} from "../../redux/find-friends-reducer";
-import FindFriends from "./FindFriendsC";
+import * as axios from "axios";
+import FindFriendsC from './FindFriendsC';
+
+
+class FindFriendsApi extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    
+    clickPage(elem) {
+        this.props.setPage(elem)
+        console.log(elem)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.countPage}&page=${elem}`).then(response => {
+            this.props.setUsers(response.data)
+        })
+    }
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.countPage}&page=${this.props.currentPage}`).then(response => {
+            this.props.setUsers(response.data)
+            console.log(response)
+        })
+    }
+
+    render() {
+        
+        return (
+            
+            <FindFriendsC totalCount={this.props.totalCount}
+                        countPage={this.props.countPage}
+                        currentPage={this.props.currentPage}
+                        clickPage={this.clickPage}
+                        users={this.props.users}
+                        unFollow={this.props.unFollow} 
+                        follow={this.props.follow}/>
+                        
+        )               
+    }
+}
 
 let mapStateToProps = (store) => {
     
@@ -27,12 +67,11 @@ let mapDispatchToProps = (dispatch) => {
         setPage: (page) => {
             dispatch(setPageAC(page))
         }
-    }
-        
+    }    
 }
 
 
 
-const FindFriendsContainer = connect(mapStateToProps, mapDispatchToProps)(FindFriends)
 
-export default FindFriendsContainer;
+
+export default connect(mapStateToProps, mapDispatchToProps)(FindFriendsApi);
--- src/components/Navbar/FindFriends/FindFriendsC.jsx
+++ src/components/Navbar/FindFriends/FindFriendsC.jsx
@@ -1,73 +1,53 @@
-import * as axios from "axios";
 import React from "react";
 import classes from './FindFriends.module.css';
 import usersPhoto from '../../../assets/img/usersPhoto.png';
 
 
-class FindFriends extends React.Component {
-    constructor(props){
-    super(props);
-        
-}
-    clickPage(elem) {
-        this.props.setPage(elem)
-        console.log(elem)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.countPage}&page=${elem}`).then(response => {
-        this.props.setUsers(response.data)  
-        
-        })
-    }
+let FindFriends = (props) => {
+    console.log(props)
+    let pages = [];
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.countPage}&page=${this.props.currentPage}`).then(response => {
-        this.props.setUsers(response.data)  
-        console.log(response)
-        })
-    }   
-    render() {
-        
-        let pages = [];
-
-        for(let i = 1; i<=Math.ceil(this.props.totalCount/this.props.countPage); i++){
+        for (let i = 1; i <= Math.ceil(props.totalCount / props.countPage); i++) {
             pages.push(i);
         }
-        console.log('page' + this.props.currentPage)
-        return (
-            <div>
-                <div className={classes.pageNumber}>{pages.map(elem => 
-                    <span  className={this.props.currentPage === elem && classes.selectedPage}  onClick={() => this.clickPage(elem)}>{elem}</span>
-                )}</div>
+        console.log('page' + props.currentPage)
 
-                {
-                    this.props.users.map((elem) => ( 
-                        <div key={elem.id} className={classes.itemUsers}>
-                            <div>
-                                <img src={elem.photos.small != null ? elem.photos.small : usersPhoto }/>
-                            </div>
-                            <div>
-                                {elem.name}   
-                            </div>
-                            <div>
-                                Статус: {elem.status}
-                            </div>
-                            <div>
-                                <span>{'elem.location.city'}</span>
-                            </div>
-                            <div>
-                                <span>{'elem.location.country'}</span>
-                            </div>
-                            <div>
-                                {elem.followed ? <button onClick={()=>{this.props.unFollow(elem.id)}}>Убрать из друзей</button> : <button onClick={()=>{this.props.follow(elem.id)}}>Добавить в друзья</button>}
-                            </div>
+    return (
+        <div>
+            <div className={classes.pageNumber}>{pages.map(elem =>
+                <span className={props.currentPage === elem && classes.selectedPage} onClick={() => props.clickPage(elem)}>{elem}</span>
+            )}</div>
+
+            {
+                props.users.map((elem) => (
+                    <div key={elem.id} className={classes.itemUsers}>
+                        <div>
+                            <img src={elem.photos.small != null ? elem.photos.small : usersPhoto} />
                         </div>
-                        )
-                    )
-                }
-                    
-            </div>
-        )
-    }
-}
+                        <div>
+                            {elem.name}
+                        </div>
+                        <div>
+                            Статус: {elem.status}
+                        </div>
+                        <div>
+                            <span>{'elem.location.city'}</span>
+                        </div>
+                        <div>
+                            <span>{'elem.location.country'}</span>
+                        </div>
+                        <div>
+                            {elem.followed ? <button onClick={() => { props.unFollow(elem.id) }}>Убрать из друзей</button> : <button onClick={() => { props.follow(elem.id) }}>Добавить в друзья</button>}
+                        </div>
+                    </div>
+                )
+                )
+            }
+            <div className={classes.pageNumber}>{pages.map(elem =>
+                <span className={props.currentPage === elem && classes.selectedPage} onClick={() => props.clickPage(elem)}>{elem}</span>
+            )}</div>
+        </div>
+    )
+            }
 
-
-export default FindFriends;
+    export default FindFriends;
